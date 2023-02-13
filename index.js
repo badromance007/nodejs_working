9d71c639b43e4fdc2c7d7eb41d278c4b0a3ee020
Peter Nguyen <nguyenpeter.edu@gmail.com>
2023-02-14 01:00:03 +0700

    update setup firestore

--- index.js
+++ index.js
@@ -2,27 +2,36 @@ const express = require('express')
 const dotenv = require('dotenv').config() // env variables
 const port = process.env.PORT || 3500
 
+const { FieldValue } = require('firebase-admin/firestore')
+const { db } = require('./firebase.js')
+
 const app = express()
+app.use(express.json())
 
 // get path that store HTML files
 const root_path = require('path').resolve('./')
 app.set('views',  root_path + '/views')
 app.set('view engine', 'ejs');
 
-
-app.get('/api/hass', (req, res) => {
-    res.status(200).json({
-        serial_number: "1234",
-        wan_ip: "1222312321",
-    })
+app.patch('/api/hass', async (req, res) => {
+    const { wan_ip } = req.body
+    const homeRef = db.collection('home').doc('hass')
+    const res2 = await homeRef.set({
+        wan_ip: wan_ip
+    }, { merge: true })
+    res.status(200).send(res2)
 })
 
 // display index file and pass variable to it
-app.get('/', (req, res) => {
-    res.render("index", {
-        serial_number: "1234",
-        wan_ip: "1222312321",
-    })
+app.get('/', async (req, res) => {
+
+    const homeRef = db.collection('home').doc('hass')
+    const doc = await homeRef.get()
+    if (!doc.exists) {
+        return res.sendStatus(400)
+    }
+
+    res.render("index", doc.data())
 })
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
